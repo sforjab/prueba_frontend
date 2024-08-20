da34439cfe43d42dcb0f01478fd2757507982d0f
Sergio Forja Barriga <sforjab@dipucadiz.dom>
2024-08-20 13:31:17 +0200

    Código de componente cliente-dashboard

--- src/app/cliente/Components/cliente-dashboard/cliente-dashboard.component.ts
+++ src/app/cliente/Components/cliente-dashboard/cliente-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cliente-dashboard',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./cliente-dashboard.component.scss']
 })
 export class ClienteDashboardComponent {
+
+  constructor() { }
+
+  ngOnInit(): void { }
 
 }
--- src/app/cliente/cliente-routing.module.ts
+++ src/app/cliente/cliente-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClienteDashboardComponent } from './Components/cliente-dashboard/cliente-dashboard.component';
+import { ClientePerfilComponent } from './Components/cliente-perfil/cliente-perfil.component';
+import { MascotasListComponent } from './Components/mascotas/mascotas-list/mascotas-list.component';
 
 const routes: Routes = [
-  { path: '', component: ClienteDashboardComponent }
+  { path: '', component: ClienteDashboardComponent },
+  { path: 'perfil', component: ClientePerfilComponent },  // Ruta para "Mis Datos"
+  { path: 'mascotas', component: MascotasListComponent }  // Ruta para "Mis Mascotas"
 ];
 
 @NgModule({
